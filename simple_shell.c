2fa4741611c0bc8ec87edb14f9ab84c718794ba7
Fooxii <ironata07@gmail.com>
2025-12-26 12:51:17 -0400

    Added simple_shell logic, still need to figure out how to handle PATH

--- simple_shell.c
+++ simple_shell.c
@@ -1,28 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 #define MAX_INPUT 1024
-int main()
+#define MAX_ARGS  64
+
+int main(int argc, char *argv[])
 {
 char input[MAX_INPUT];
+char *args[MAX_ARGS];
+pid_t pid;
+int status;
+int i;
+
+(void)argc;
+(void)argv;
 
 while (1)
 {
-printf("simple_shell:$");
+printf("simple_shell$ ");
 fflush(stdout);
 
-if(fgets(input, MAX_INPUT, stdin) == (void *)0)
+if (fgets(input, MAX_INPUT, stdin) == NULL)
 {
-perror("fgets failed");
-continue;
+printf("\n");
+break;
 }
 
-printf("%s", input);
+input[strcspn(input, "\n")] = '\0';
 
-if (strcmp(input, "exit\n") == 0)
+if (input[0] == '\0')
+continue;
+
+i = 0;
+args[i] = strtok(input, " ");
+while (args[i] && i < MAX_ARGS - 1)
 {
+i++;
+args[i] = strtok(NULL, " ");
+}
+args[i] = NULL;
+
+if (strcmp(args[0], "exit") == 0)
 break;
+
+pid = fork();
+if (pid == 0)
+{
+
+if (execve(args[0], args, environ) == -1)
+{
+perror(argv[0]);
+exit(1);
+}
+}
+else if (pid > 0)
+{
+
+wait(&status);
+}
+else
+{
+perror("fork failed");
 }
 }
 
