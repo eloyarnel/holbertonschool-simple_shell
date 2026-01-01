192534ca7630ecd40cade2827bf25f4abf0a78c5
Fooxii <ironata07@gmail.com>
2026-01-01 14:55:03 -0400

    UNFINISHED:  handles function that frees all arguments

---
+++ free_args.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * free_args - function that frees all arguments provided from stdin
+ * @args: pointer to array of strings that contain all arguments being freed
+ * Return: void
+ */
+
+void free_args(char **args)
+{
+
+}
