ee95e0d3133b8ffeb34794047b5499613cc714aa
Fooxii <ironata07@gmail.com>
2026-01-08 18:59:30 -0400

    BUG FIX: run_command wasn't working with build_cmd_path

--- run_command.c
+++ run_command.c
@@ -1,25 +1,40 @@
 #include "shell.h"
 
 /**
- * run_command - function that runs the command given in
- * args[0] along with its arguments if any
+ * run_command - function that runs the command given
  * @args: pointer to an array of strings that have all arguments given
  * Return: void 
  */
+
 void run_command(char **args)
 {
 pid_t pid;
 int status;
+char *cmd_path;
+
+if (!args || !args[0])
+{
+return;
+}
+
+cmd_path = build_cmd_path(args[0]);
+if (!cmd_path)
+{
+fprintf(stderr, "%s: command not found\n", args[0]);
+return;
+}
 
 pid = fork();
 if (pid == 0)
 {
-execve(args[0], args, environ);
+execve(cmd_path, args, environ);
 perror("execve");
+free(cmd_path);
 exit(1);
 }
 else if (pid > 0)
 {
 wait(&status);
+free(cmd_path);
 }
 }
